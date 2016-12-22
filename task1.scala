f0ab75fc6d0770484a1bb3056389fd68e1f59d39
U-b-1799\Slava <slava_agent@mail.ru>
2016-12-22 13:28:08 +0300

    added first and second task

---
+++ task1.scala
@@ -0,0 +1,47 @@
+﻿object code {
+  /*
+  ==================
+  fib
+  
+  	Рекурсия
+  ==================
+  */
+  def fib(n: BigInt): BigInt =
+    if (n > 1) fib(n - 1) + fib(n - 2)
+    else n
+
+  /*
+  ==================
+  fibWithTailRec
+  	
+  	Обёртка для удобства вызова
+  ==================
+  */  
+  def fibWithTailRec(n: BigInt) =
+    if (n > 1) fibIter(1, 1, n - 2)
+    else n
+
+  /*
+  ==================
+  fibIter
+  
+  	Хвостовая рекурсия
+  ==================
+  */ 
+  private def fibIter(prev: BigInt, current: BigInt, n: BigInt): BigInt = 
+    if (n == 0) current
+    else fibIter(current, prev + current, n - 1)
+  
+  /*
+  ==================
+  main
+  ==================
+  */ 
+  def main(args: Array[String]) {
+      println("fibWithTailRec(1000) = " + fibWithTailRec(1000));  //Ok
+      println("fibWithTailRec(2) = " + fibWithTailRec(2));
+      
+      println("fib(2) = " + fib(2));
+      println("fib(1000) = " + fib(1000));   //Not Ok
+  }
+}
---
+++ task2.scala
@@ -0,0 +1,37 @@
+﻿import scala.List
+
+object code {
+  
+  /*
+  ==================
+  append
+  	
+  	Добавляет список b в конец списка a с помощью foldLeft
+  ==================
+  */
+  def append[ListType](a: List[ListType], b: List[ListType]) = 
+    a.reverse.foldLeft(b){ (x, y) => y :: x }
+  
+  /*
+  ==================
+  main
+  ==================
+  */ 
+  def main(args: Array[String]) {
+    val l1 = List(1, 2, 3, 4, 5)
+    val l2 = List(6, 7, 8, 9, 10)
+    
+    println("List 1 = " + l1)
+    println("List 2 = " + l2)
+    
+    println("append(l1, l2) = " + append(l1, l2))
+    
+    val l3 = List()
+    val l4 = List()
+    
+    println("List 3 = " + l3)
+    println("List 4 = " + l4)
+    
+    println("append(l3, l4) = " + append(l3, l4))
+  }
+}
